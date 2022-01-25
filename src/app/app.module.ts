316d3608d61d7a12e3800c3bde5ebcaf88157bf6
Hlulani <hlullyr@gmail.com>
2022-01-25 22:51:36 +0100

    Added navbar and carousel component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuItemsComponent } from './menu-items/menu-items.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuItemsComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
